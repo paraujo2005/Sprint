a92ba0563643717aea61f9a6170727454879c223
Pedro Araujo <pedroredn@gmail.com>
2024-10-28 21:38:29 -0300

    Corrigindo Codigos

--- CSV/POF/POF_Alimento_Rural_Urbano.py
+++ CSV/POF/POF_Alimento_Rural_Urbano.py
@@ -1,138 +1,142 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[121]:
+# In[ ]:
+
+
+pip install selenium pandas openpyxl minio webdriver-manager
+
+
+# In[133]:
 
 
 import os
 import time
-import io
+import logging
 import pandas as pd
-import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 from minio import Minio
+from io import BytesIO
 
-
-# In[123]:
-
+# Configuração de logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 minio_client = Minio(
-    "localhost:9000",  # substitua por seu URL MinIO (e.g., "localhost:9000")
+    "localhost:9000", 
     access_key="minioadmin",
     secret_key="minioadmin",
-    secure=False  # ou True se usar SSL
+    secure=False
 )
 bucket_name = "pof"
 
-# Caminho para a sua pasta "IDH" dentro de "OneDrive\Documentos"
-pasta_download = r'C:\Users\pedro\OneDrive\Documentos\POF'
-
-# Configuração do Chrome para ignorar downloads inseguros e definir o diretório de download
+# Configuração do Chrome
 chrome_options = webdriver.ChromeOptions()
-prefs = {
-    'download.default_directory': pasta_download,  # Define o diretório de download para a pasta IDH
-    'download.prompt_for_download': False,
-    'directory_upgrade': True,
-    'safebrowsing.enabled': False,
-    'safebrowsing.disable_download_protection': True  # Ignora o bloqueio de downloads inseguros
-}
-chrome_options.add_experimental_option('prefs', prefs)
-
-# Adiciona argumentos para permitir conteúdo inseguro
 chrome_options.add_argument("--allow-running-insecure-content")
 chrome_options.add_argument("--unsafely-treat-insecure-origin-as-secure=https://sidra.ibge.gov.br/pesquisa/pof/tabelas")
+#chrome_options.add_argument("--headless")  # Se não precisar de interface gráfica
 
-# Caminho para o ChromeDriver
-service = Service(r'C:\Users\pedro\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe')
+# Usando o ChromeDriverManager para garantir que o ChromeDriver esteja disponível
+service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
-# Acessar o site
-driver.get('https://sidra.ibge.gov.br/pesquisa/pof/tabelas')
+try:
+    logging.info("Acessando o site do IBGE...")
+    driver.get('https://sidra.ibge.gov.br/pesquisa/pof/tabelas')
 
+    # Entrando no Site dos Dados
+    logging.info("Acessando Dados...")
+    botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[2]/div[2]/section/div[1]/table/tbody/tr[3]/td[3]/a')
+    botao.click()
 
-# ## 3047
+    time.sleep(7)
 
-# In[125]:
+    logging.info("Selecionando opções...")
+    #Aumentar Decimos
+    botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[3]/div/div[2]/div[3]/div/div[2]/div/div/div/div/div[1]/div/div/span/button[2]')
+    botao.click()
 
+    #Selecionar Area
+    botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[4]/div/div[2]/div[3]/div/div[1]/div[1]/div/button[1]')
+    botao.click()
 
-nome_arquivo = "POF_Alimento_Rural_Urbano"
+    #Selecionar Grupos
+    botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[5]/div/div[2]/div[3]/div/div[1]/div[1]/div/button[1]')
+    botao.click()
 
-#Entrar no link
-botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[2]/div[2]/section/div[1]/table/tbody/tr[3]/td[3]/a')
-botao.click()
+    #Selecionar Região
+    botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[7]/div/div[2]/div[3]/div/div/div/div[1]/div[2]/ul[1]/li[2]/div/div/div/button')
+    botao.click()
 
-time.sleep(10)
+    time.sleep(5)
+    
+    # Localize o botão de download e clique nele
+    logging.info("Clicando no botão de download 1...")
+    botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[5]/div[2]/div/div[2]/button[2]')
+    botao.click()
 
-#Aumentar Decimos
-botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[3]/div/div[2]/div[3]/div/div[2]/div/div/div/div/div[1]/div/div/span/button[2]')
-botao.click()
+    time.sleep(5)
+    
+    # Definindo nome do arquivo
+    logging.info("Definindo nome...")
+    campo_input = driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div[2]/div/div/div[1]/table/tbody/tr[1]/td[2]/input')
+    campo_input.send_keys("arquivo_para_minio")
 
-#Selecionar Area
-botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[4]/div/div[2]/div[3]/div/div[1]/div[1]/div/button[1]')
-botao.click()
+    # Localize o botão de download e clique nele
+    logging.info("Clicando no botão de download 2...")
+    botao = driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div[2]/div/div/div[2]/a')
+    botao.click()
 
-#Selecionar Grupos
-botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[5]/div/div[2]/div[3]/div/div[1]/div[1]/div/button[1]')
-botao.click()
+    # Aguardar o download ser concluído (ajuste conforme necessário)
+    logging.info("Aguardando o download ser concluído...")
+    time.sleep(15)
 
-#Selecionar Região
-botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[4]/div[7]/div/div[2]/div[3]/div/div/div/div[1]/div[2]/ul[1]/li[2]/div/div/div/button')
-botao.click()
-
-time.sleep(5)
-
-#Clicar Download
-botao = driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div[1]/div[5]/div[2]/div/div[2]/button[2]')
-botao.click()
-
-time.sleep(5)
-
-input_element = driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div[2]/div/div/div[1]/table/tbody/tr[1]/td[2]/input')
-input_element.send_keys(nome_arquivo)
-
-botao = driver.find_element(By.XPATH, '/html/body/div[7]/div/div/div[2]/div/div/div[2]/a')
-botao.click()
-
-time.sleep(20)
-
-# Verifique se o arquivo foi baixado
-arquivos_na_pasta = os.listdir(pasta_download)
-arquivo_xlsx = [arquivo for arquivo in arquivos_na_pasta if arquivo.endswith('.xlsx')]
-        
-if arquivo_xlsx:
-    # Converter .xlsx para .csv usando Pandas
-    caminho_arquivo = os.path.join(pasta_download, arquivo_xlsx[0])
-    df = pd.read_excel(caminho_arquivo)
-    arquivo_csv = nome_arquivo + '.csv'
-    df.to_csv(os.path.join(pasta_download, arquivo_csv), index=False)
-
-    # Enviar o arquivo CSV para o MinIO
+    # Verifique se o arquivo foi baixado
+    arquivo_xlsx = None
     try:
-        # Cria o bucket caso não exista
-        if not minio_client.bucket_exists(bucket_name):
-            minio_client.make_bucket(bucket_name)
-        
-        # Caminho completo do CSV
-        caminho_csv = os.path.join(pasta_download, arquivo_csv)
-        
-        # Upload para o MinIO
-        minio_client.fput_object(bucket_name, arquivo_csv, caminho_csv)
-        print(f'O arquivo {arquivo_csv} foi enviado com sucesso para o bucket {bucket_name} no MinIO!')
+        # Verificando a presença do arquivo na pasta de downloads
+        downloads_dir = os.path.join(os.path.expanduser("~"), "Downloads")  # Diretório padrão de downloads
+        arquivo_xlsx = [f for f in os.listdir(downloads_dir) if f == "arquivo_para_minio.xlsx"]
     except Exception as e:
-        print(f"Erro ao enviar para o MinIO: {e}")
-    finally:
-        # Remover os arquivos locais após o upload
-        os.remove(caminho_arquivo)
-        os.remove(caminho_csv)
-else:
-    print("Erro: Arquivo .xlsx não encontrado.")
+        logging.error(f"Erro ao listar os arquivos na pasta de downloads: {e}")
 
-time.sleep(5)
+    if arquivo_xlsx:
+        logging.info(f"Arquivo encontrado: {arquivo_xlsx[0]}")
 
-driver.quit()
+        # Ler o arquivo XLSX diretamente para um DataFrame
+        caminho_arquivo = os.path.join(downloads_dir, arquivo_xlsx[0])
+        df = pd.read_excel(caminho_arquivo)
+
+        # Converter o DataFrame para CSV
+        logging.info("Convertendo o arquivo XLSX para CSV...")
+        csv_buffer = BytesIO()
+        df.to_csv(csv_buffer, index=False)
+        csv_buffer.seek(0)  # Retorna ao início do buffer
+
+        # Enviar o arquivo CSV para o MinIO
+        if not minio_client.bucket_exists(bucket_name):
+            logging.info(f"Criando bucket: {bucket_name}")
+            minio_client.make_bucket(bucket_name)
+
+        # Upload do arquivo CSV
+        logging.info(f"Enviando o arquivo CSV para o bucket {bucket_name} no MinIO...")
+        minio_client.put_object(bucket_name, 'POF_Alimento_Rural_Urbano.csv', csv_buffer, csv_buffer.getbuffer().nbytes)
+        logging.info(f'O arquivo POF_Alimento_Rural_Urbano.csv foi enviado com sucesso para o bucket {bucket_name} no MinIO!')
+    else:
+        logging.error("Erro: Nenhum arquivo XLSX encontrado.")
+
+except Exception as e:
+    logging.error(f"Erro ao processar o arquivo: {e}")
+
+finally:
+    # Finalize o Selenium
+    logging.info("Apagando Arquivo...")
+    os.remove("arquivo_para_minio.xlsx")
+    logging.info("Fechando o navegador...")
+    driver.quit()
 
 
 # In[ ]:
